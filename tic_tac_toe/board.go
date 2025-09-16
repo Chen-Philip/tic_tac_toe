f3858465375163f53459ea3ca777622703302aa2
Philip Chen <philip.chen.0223@gmail.com>
2025-09-16 14:20:11 -0400

    Made basic tic tac toe board

---
+++ tic_tac_toe/board.go
@@ -0,0 +1,23 @@
+package tic_tac_toe
+
+import (
+	"fmt"
+)
+
+type Board struct {
+	board [3][3]int
+}
+
+func (b Board) PrintBoard() {
+	for i, row := range b.board {
+		for j, cell := range row {
+			fmt.Print(" ", cell, " ")
+			if j < len(row)-1 {
+				fmt.Print("|")
+			}
+		}
+		if i < len(b.board)-1 {
+			fmt.Println("\n---+---+---")
+		}
+	}
+}
