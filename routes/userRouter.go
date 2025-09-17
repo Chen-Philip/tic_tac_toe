58b72b8b9c1a17c49de3d1c9ea43e85b532bdec4
Philip Chen <philip.chen.0223@gmail.com>
2025-09-17 16:58:09 -0400

    Added routes

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,16 @@
 package routes
+
+import (
+	controller "chess/controllers"
+	"chess/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+// UserRoutes We are using middleware here to ensure that both routes here are protected. Authroutes aren't protected because
+//
+//	the user does't have a token yet. After loging, theyll have a token that they should use.
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
