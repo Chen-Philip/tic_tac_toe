1aa1e2ca0448208749559a539c3e73cb227bbea4
Philip Chen <philip.chen.0223@gmail.com>
2025-09-17 14:57:55 -0400

    Finished database setup logic

---
+++ database/databaseConnection.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+	"time"
+)
+
+func DBinstance() *mongo.Client {
+	// Loads environment variables from the .env file and returns an error if one occurs
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// os.Getenv("MONGODB_URL") reads the environmental variable "MONGODB_URL" and returns it as a string
+	MongoDb := os.Getenv("MONGODB_URL")
+
+	// Set a timeout context for the connection (context is a messenger, so in this case
+	// it lets the API know to cancel after 10 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+	return client
+}
+
+var Client *mongo.client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	return client.Database("cluster0").Collection(collectionName)
+}
